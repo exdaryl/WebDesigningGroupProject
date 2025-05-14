f0952aa19ae3ee397087949f203c411b1b7f7003
exdaryl <117067180+exdaryl@users.noreply.github.com>
2025-05-14 19:08:53 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// JavaScript for the toggle menu button in all the webpages
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
